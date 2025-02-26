0bf2b26fbc93edfddc78323f49e83cfd6e3aa3e0
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2025-02-26 17:44:21 -0500

    creates get project by id handler

--- src/api/projects/projects.go
+++ src/api/projects/projects.go
@@ -8,7 +8,7 @@ func RegisterRoutes(router *gin.Engine) {
 	projectGroup := router.Group("/api/projects")
 	{
 		projectGroup.GET("/", getAllProjectsHandler)
-		// projectGroup.GET("/:id", GetProjectByID)
+		projectGroup.GET("/:id", getProjectByIdHanlder)
 		// projectGroup.POST("/", middleware.Auth, middleware.ProcessFileUploads, CreateProject)
 		// projectGroup.PUT("/:id", middleware.Auth, middleware.ProcessFileUploads, UpdateProject)
 		// projectGroup.PATCH("/:id", middleware.Auth, DeleteProject)
--- src/api/projects/projects_handler.go
+++ src/api/projects/projects_handler.go
@@ -20,3 +20,20 @@ func getAllProjectsHandler(c *gin.Context) {
 		"data":    projects,
 	})
 }
+
+func getProjectByIdHanlder(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "No Project ID provided"})
+		return
+	}
+	fetchedProject, err := getProjectById(id, db.DB)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Project not found"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Project found!",
+		"data":    fetchedProject,
+	})
+}
